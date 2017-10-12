182b1749b6b6acde7de40d6093731ebf08f212cf
Eleonora <eleonora.racca@cern.ch>
2017-10-12 13:01:39 +0200

    Ele: caricamento secondo esercizio sui decadimenti

---
+++ esercizi/CriEle/Poissoniana.C
@@ -0,0 +1,116 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <Riostream.h>
+#include <TCanvas.h>
+#include <TMath.h>
+#include <TH1F.h>
+#include <TF1.h>
+#include <TFile.h>
+#include <TRandom.h>
+#include <TRandom3.h>
+#include <TVirtualFitter.h>
+
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+#endif
+
+using namespace std;
+using namespace TMath;
+
+// Dichiarazioni
+double Decadimento(int ntot, const float deltat, const float ttot, const float alfa);
+double Binomiale(const int nesperimenti, const int nintervalli, float beta, float tempo);
+double Poisson(const int nesperimenti, float beta, float tempo);
+double LogGamma();
+void Poissoniana(int ntot = 1000, const float deltat = 1., const float ttot = 100., const float alfa = 2.5e-5, const int nesp = 1000);
+
+// Implementazioni
+void Poissoniana(int ntot, const float deltat, const float ttot, const float alfa, const int nesp){
+
+  const int dimensione = (int)ttot/deltat;
+  const int tempotot = dimensione*deltat;
+  const float Ntot = ntot;
+  const float beta = ntot*alfa;
+  const int nbin = Ntot*0.01 + 1;
+  double Nrest;
+  
+  TH1F *histo = new TH1F("histo", "Numero di decadimenti", nbin, - 0.5, nbin - 0.5);
+
+  ///*
+  for(int i = 0; i < nesp; i++){
+    if(i%100 == 0){
+      cout << "Sto elaborando il decadimento numero " << i << endl;
+    }
+    Nrest = Decadimento(ntot, deltat, ttot, alfa);
+    histo->Fill(Nrest);
+  }
+  //*/
+  /*
+  for(int k = 0; k < nesp; k++){
+    if(k%100 == 0){
+      cout << "Sto elaborando il decadimento numero " << k << endl;
+    }
+    int Nrest = 0;
+    
+    for(int i = 0; i < dimensione; i++){
+      int Ndec = 0;
+      
+      if(i != 0){
+       	for(int j = 0; j < ntot; j++){
+	  if(gRandom->Rndm() <= alfa*deltat)
+	    Ndec++;
+	    }
+      }
+      
+      ntot = ntot - Ndec;
+    }
+    
+    Nrest = (int)Ntot - ntot;
+    histo->Fill((double)Nrest);
+  }
+  */
+  
+  cout << "Ho finito, plotto il grafico." << endl;
+  TCanvas *chisto = new TCanvas("chisto", "Numero di decadimenti", 10, 10, 1280, 1024);
+  histo->Draw();
+  
+}
+
+// /*
+double Decadimento(int ntot, const float deltat, const float ttot, const float alfa){
+  const int dimensione = (int)ttot/deltat;
+  const int numeroiniziale = ntot;
+  
+  for(int i = 0; i < dimensione; i++){
+    int Ndec = 0;
+    
+    for(int j = 0; j < ntot; j++){
+      if(gRandom->Rndm() <= alfa*deltat)
+	Ndec++;
+    }
+    
+    ntot = ntot - Ndec;
+  }
+
+  return (double)numeroiniziale - ntot;
+}
+// */
+
+// /*
+double Binomiale(const int nesperimenti, const int nintervalli, float beta, float tempo){
+  probabilita = beta*tempo;
+  return 1.;
+}
+// */
+
+// /*
+double Poisson(const int nesperimenti, float beta, float tempo){
+  probabilita = beta*tempo;
+  return Exp(nesperimenti * Log(probabilita) - probabilita - LnGamma(nesperimenti + 1));
+}
+// */
+
+// /*
+double LogGamma(){
+  return 1.;
+}
+// */
