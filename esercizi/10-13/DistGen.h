8457cc737f8826cd59d834601e7abb2df3369cb8
lorenzodecilladi <lorenzo.decilladi@edu.unito.it>
2017-10-13 16:38:22 +0200

    Distribution generator header file

--- esercizi/10-13/DistGen.h
+++ esercizi/10-13/DistGen.h
@@ -1,17 +1,30 @@
 #ifndef DISTGEN_H
 #define DISTGEN_H
 
-#include <TH1F.h>
-#include <TF1.h>
-#include <TMath.h>
 #include <TRandom3.h>
-
+#include <TMath.h>
 
 class DistGen : public TRandom3{
 
 public:
 
 DistGen();
+DistGen(double alpha, double xmin=0., double xmax=2*TMath::Pi(), double seed=65349);
+virtual ~DistGen();
+DistGen& operator=(const DistGen& source); //& prima di operator: non crea una
+
+double func(double xx);
+
+double inversion();
+double rejection();
+
+
+private:
+
+double fParam;
+double fXmin;
+double fXmax;
+unsigned int fSeed;
 
 
 };
