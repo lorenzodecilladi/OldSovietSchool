e48e7cdf929fa601cd66ff537c21d8b3c6ce55dc
lorenzodecilladi <lorenzo.decilladi@edu.unito.it>
2017-10-13 10:53:09 +0200

    prova

---
+++ esercizi/10-13/DistGen.h
@@ -0,0 +1,20 @@
+#ifndef DISTGEN_H
+#define DISTGEN_H
+
+#include <TH1F.h>
+#include <TF1.h>
+#include <TMath.h>
+#include <TRandom3.h>
+
+
+class DistGen : public TRandom3{
+
+public:
+
+DistGen();
+
+
+};
+
+
+#endif
