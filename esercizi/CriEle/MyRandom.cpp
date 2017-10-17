6d7f74047b36675aafa93c4988f849ede8e9d867
Eleonora <eleonora.racca@cern.ch>
2017-10-17 12:42:31 +0200

    Eleonora: fine implementazione member function

--- esercizi/CriEle/MyRandom.cpp
+++ esercizi/CriEle/MyRandom.cpp
@@ -1,25 +1,53 @@
 #include <Riostream.h>
 #include "MyRandom.h"
 #include "TString.h"
+#include "TMath.h"
 
 ClassImp(MyRandom)
 
 MyRandom::MyRandom():
   TRandom(),
-  fNestrazioni(0),
-  fParametroa(0.)
+  fMaggiorante(0.),
+  fParametroa(0.),
+  fPi(0.),
+  fRadiceParametro(0.)
 {}
-  
-MyRandom::MyRandom(Int_t nestrazioni, Double_t parametroa):
+
+MyRandom::MyRandom(Double_t parametroa):
   TRandom(),
-  fNestrazioni(nestrazioni),
-  fParametroa(parametroa)
-{}
+  fMaggiorante(0.),
+  fParametroa(parametroa),
+  fPi(0.),
+  fRadiceParametro(0.)
+{
+  SetName("MyRandom");
+  SetTitle("Random number generator: Mersenne Twister");
+
+  Inizializzazione();
+}
+
+MyRandom::Inizializzazione():
+{
+  fPi = TMath::Pi();
+  if(fParametroa <= 0. || fParametroa > 1.){
+    Warning("Inizializzazione","Parametro a non valido = %f8.3 \n",fParametroa);
+    Warning("Init","Bisognerebbe inizializzarlo appropriatamente\n");
+    fParametroa = 0.;
+    fMaggiorante = 0.;
+    fRadiceParametro = 0.;
+  }
+  else {
+    fMaggiorante = 1/fParametroa;
+    fRadiceParametro = TMath::Sqrt(fParametroa);
+  }
+}
   
 MyRandom::MyRandom(const MyRandom &source):
   TRandom(source),
-  fNestrazioni(source.fNestrazioni),
-  fParametroa(source.fParametroa)
+  fMaggiorante(source.fMaggiorante),
+  fParametroa(source.fParametroa),
+  fPi(source.fPi),
+  fRadiceParametro(source.fRadiceParametro)
 {}
 
 MyRandom::~MyRandom()
@@ -28,19 +56,13 @@ MyRandom::~MyRandom()
 MyRandom& MyRandom::operator=(const MyRandom& source){
   if(this != &source){
     TRandom::operator=(source);
-    fNestrazioni = source.fNestrazioni;
+    fMaggiorante = source.fMaggiorante;
     fParametroa = source.fParametroa;
+    fPi = source.fPi;
+    fRadiceParametro = source.fRadiceParametro;
   }
 
   return *this;
-}
-
-MyRandom::SetEstrazioni(Int_t nestrazioni){
-  fNestrazioni = nestrazioni;
-}
-
-Int_t MyRandom::GetEstrazioni(){
-  return fNestrazioni;
 }
 
 MyRandom::SetA(Double_t parametroa){
@@ -50,3 +72,59 @@ MyRandom::SetA(Double_t parametroa){
 Double_t MyRandom::GetA(){
   return fParametroa;
 }
+
+Double_t Funzione1(double teta)
+{
+  double senquadro = TMath::Sin(teta) * TMath::Sin(teta);
+  double cosquadro = TMath::Cos(teta) * TMath::Cos(teta);
+
+  return 1/(senquadro + fParametroa*cosquadro);
+}
+
+Double_t Funzione2(double teta)
+{
+  double senquadro = TMath::Sin(teta) * TMath::Sin(teta);
+  double tetaquadro = teta*teta;
+
+  return senquadro/tetaquadro;
+}
+
+Double_t F1Reiezione1()
+{
+  double x, y;
+
+  do{
+    x = 2 * fPi * Rndm();
+    y = Rndm() * fMaggiorante;
+  }while(Funzione1(x) >= y);
+
+  return x;
+}
+
+Double_t F1Reiezione2()
+{
+  double x = 2. * Rndm() * fPi;
+  double y = Rndm() * fMaggiorante;
+  
+  if(Funct1(x) >= y)
+    return x;
+  else
+    return F1Reiezione2();
+}
+
+Double_t F1Inversione()
+{
+  double u = Rndm();
+  double teta = TMath::ATan(fRadiceParametro * TMath::Tan(fPi * u - fPi/2));
+
+  double w = Rndm();
+  if(w < 0.5){
+    teta = teta + fPi;
+  }
+  else if(teta < 0){
+    teta = teta + 2*fPi;
+  }
+
+  return teta;
+}
+
