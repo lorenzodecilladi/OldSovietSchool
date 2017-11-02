fed4ddadc7a4ecb1580d86c2eb86b609a9ab2b53
lorenzodecilladi <lorenzo.decilladi@edu.unito.it>
2017-11-02 10:57:00 +0100

    chi2 update

--- esercizi/10-31/chi_2_MC.C
+++ esercizi/10-31/chi_2_MC.C
@@ -6,61 +6,79 @@
 #include <TMath.h>
 #include <TRandom3.h>
 #include <TCanvas.h>
+#include <TLine.h>
 #endif
 
 
-
+//============= FUNCTION DECLARATION ==============
 double poisson(double *x, double *par);
-double chi2(double *x, double *mu, int N);
+double chi2(double *x, double *mu, int N); //evaluates chi2
 double rootInversion(TF1* funcPtr);
-Double_t ChiSquareDistr(Double_t *x,Double_t *par);
+Double_t chi2PDF(Double_t *x,Double_t *par); //PDF Chi2
   
 
 
 
+//======== MAIN ========
+void chi_2_MC(const int nrep = 100){ // nrep = number of repetitions
 
-void chi_2_MC(const int nloop = 1000){
+  const int nDoF = 12;   //number of DoF = nbins of background histogram
 
-  const int nbins = 12;
-  double data[nbins] = {1., 3., 1., 4., 3., 5., 2., 1., 1., 0., 1., 3.};
-  double mu[nbins];
+  //background
   TCanvas *c1 = new TCanvas();
   c1->cd();
-  TH1D *histData = new TH1D("histData","Istogramma dati osservati",nbins,0.,24.);
-  for(int i=0; i<nbins; i++){
+  TH1D *histData = new TH1D("histData","Istogramma dati osservati",nDoF,0.,24.);
+  TF1 *teoBg = new TF1("teoBg", "1000*0.169*(10-0.01*x)",0.,24.); //expected background
+  
+  double data[nDoF] = {1., 3., 1., 4., 3., 5., 2., 1., 1., 0., 1., 3.}; //"observed" data
+  double mu[nDoF]; //expected values  
+  
+  for(int i=0; i<nDoF; i++){
+    data[i] = 1000.*data[i];
     histData -> Fill(2*i+1,data[i]);
-    mu[i] = 0.169*(10-0.01*(2*i+1));
+    mu[i] = 1000*0.169*(10-0.01*(2*i+1));
   }
   histData-> Draw("hist");
+  teoBg->Draw("same");
 
-
-  TF1 *expBg = new TF1("expBg", "0.169*(10-0.01*x)",0.,24.);
-  expBg->Draw("same");
-
-
-  cout << chi2(data,mu,nbins) <<endl;
-  
 
   TCanvas *c2 = new TCanvas();
   c2->cd();
-  TH1D *histChi2 = new TH1D("histChi2","Istogramma chi quadri MC",30,0.,60.);
-  TF1 *fPoisson = new TF1("fPoisson", poisson, 0., 24, 1);
-  double x[nbins];
-  
-  for(int j=0; j<nloop; j++){
-    for(int i=0; i<nbins; i++){
-      fPoisson->SetParameter(0, mu[i]);
-      x[i]= rootInversion(fPoisson);
-    }
-    histChi2->Fill(chi2(x,mu,nbins));
-  }
-  histChi2->Draw();
 
-  TF1 *pearson = new TF1("pearson", ChiSquareDistr, 0., 60., 2);
-  pearson -> SetParameter(0, nbins);
-  pearson -> SetParameter(1, histChi2->Integral(0, nbins));
-  pearson -> Draw("same");
+  //CHI2 observed data
+  double chi2Data = chi2(data,mu,nDoF); //(observed data, expected values, nDoF)
+  cout << "Chi2 from observed data: " << chi2Data <<endl;
+  TLine *lineChi2 = new TLine(chi2Data, 0, chi2Data, nrep/10);
+  lineChi2 -> SetLineColor(kOrange-2);
+  lineChi2 -> SetLineWidth(3);
+
+  //CHI2 MC data
+  double maxChi2 = 60.;
+  int nbinsChi2 = 30;
+  double binSizeChi2 = maxChi2/nbinsChi2;
+  TH1D *histChi2 = new TH1D("histChi2","Istogramma chi2 MC",nbinsChi2,0.,maxChi2);
+  TF1 *fPoisson = new TF1("fPoisson", poisson, 0., mu[0]+10, 1); //24.
+  double dataMC[nDoF];
   
+  for(int j=0; j<nrep; j++){ //loop over experiment repetitions
+    for(int i=0; i<nDoF; i++){ //loop over data histogram bins
+      fPoisson->SetParameter(0, mu[i]); //MC extraction
+      dataMC[i] = rootInversion(fPoisson);
+    }
+    histChi2->Fill(chi2(dataMC,mu,nDoF));
+    if(j%100==0) {cout<<"Processing event " << j << endl;}
+  }
+  histChi2->SetMarkerStyle(22);
+  histChi2->SetMarkerSize(1.5);
+  histChi2->Draw("hist");
+  
+  TF1 *pearson = new TF1("pearson", chi2PDF, 0., 60., 2);
+  pearson -> SetParameter(0, nDoF);
+  pearson -> SetParameter(1, binSizeChi2*histChi2->Integral(0, nbinsChi2));
+  cout << histChi2->Integral(0, nbinsChi2) << endl;
+
+  pearson -> Draw("same");
+  lineChi2 -> Draw("same");
 }
 
 
@@ -92,18 +110,18 @@ double rootInversion(TF1* funcPtr){
 }
 
 
-Double_t ChiSquareDistr(Double_t *x,Double_t *par)
+Double_t chi2PDF(Double_t *x,Double_t *par)
 {
-// Chisquare density distribution for nrFree degrees of freedom
-
-Double_t nrFree = par[0];
-Double_t Int = par[1];
-Double_t chi2 = x[0];
-
-if (chi2 > 0) {
-Double_t lambda = nrFree/2.;
-Double_t norm = TMath::Gamma(lambda)*TMath::Power(2.,lambda);
-return Int*TMath::Power(chi2,lambda-1)*TMath::Exp(-0.5*chi2)/norm;
-} else
-return 0.0;
+  // Chisquare density distribution for nrFree degrees of freedom
+  
+  Double_t nrFree = par[0];
+  Double_t Int = par[1];
+  Double_t chi2 = x[0];
+  
+  if (chi2 > 0) {
+    Double_t lambda = nrFree/2.;
+    Double_t norm = TMath::Gamma(lambda)*TMath::Power(2.,lambda);
+    return Int*TMath::Power(chi2,lambda-1)*TMath::Exp(-0.5*chi2)/norm;
+  } else
+    return 0.0;
 }
