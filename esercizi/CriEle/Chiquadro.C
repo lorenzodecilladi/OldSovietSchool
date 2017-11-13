bb41d4215a80390e630dca2579c2aeec5ebd3134
Eleonora <eleonora.racca@cern.ch>
2017-11-13 13:52:43 +0100

    Ele: piccole modifiche all'esercizio sul chi quadro, non funziona

--- esercizi/CriEle/Chiquadro.C
+++ esercizi/CriEle/Chiquadro.C
@@ -6,6 +6,7 @@
 #include <TH1D.h>
 #include <TLine.h>
 #include <TMath.h>
+#include <TRandom.h>
 #include <TRandom3.h>
 #endif
 
@@ -16,23 +17,27 @@ using namespace TMath;
 void Chiquadro(); //Funzione da chiamare per eseguire la macro
 double Fondo2(int x);
 double CalcolaChi(int n[], double nu[], const int lunghezza);
-double PearsonDist(double x, int gradiliberta);
+double PearsonDist(double x, int gradiliberta, double norm);
+//double PearsonDist(double x, int gradiliberta);
 
 // ~~~~~~~~~~~~ Implementazioni ~~~~~~~~~~~~
 void Chiquadro(){
   // dichiarazioni e inizializzazione delle variabili
   const int nbin = 12;
-  const int nesperimenti = 1e7;
+  const int nesperimenti = 1e4;
+  const double xmin = 0.;
+  const double xmax = 90.;
+  const int nbinchi = 90;
+  const double larghezza = (xmax - xmin)/nbinchi;
   
   const int dati[nbin] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
   int pesi[nbin] = {1, 3, 1, 4, 3,  5,  2,  1,  1,  0,  1,  3};
   double attesi[nbin];
   
   TF1 *Fondo = new TF1("Fondo", "0.169 * (10 - 0.01 * x)", 0, 24);
-  TF1 *Pearson = new TF1("Pearson", "PearsonDist(x, 12)", 0, 90);
  
-  TH1I *originale = new TH1I("originale", "Dati originali", 12, 0, 24);
-  TH1D *chiquadro = new TH1D("chiquadro", "Distribuzione del #chi^{2}", 100, 0, 90);
+  TH1I *originale = new TH1I("originale", "Dati originali", nbin, 0, 24);
+  TH1D *chiquadro = new TH1D("chiquadro", "Distribuzione del #chi^{2}", nbinchi, xmin, xmax);
 
   // Dati originali: calcolo del Chi quadro e disegno dell'istogramma
   for(int i = 0; i < nbin; i++){
@@ -55,12 +60,17 @@ void Chiquadro(){
     double chiquadroesperimenti = 0.;
     
     for(int j = 0; j < nbin; j++){
-      datiesperimenti[j] = Poisson(dati[j], attesi[j]);
+      datiesperimenti[j] = gRandom->TRandom::Poisson(attesi[j]);
     }
 
     chiquadroesperimenti = CalcolaChi(datiesperimenti, attesi, nbin);
     chiquadro->Fill(chiquadroesperimenti);
   }
+
+  double integrale = larghezza*chiquadro->Integral(xmin, xmax);
+  cout << integrale << endl;
+  TF1 *Pearson = new TF1("Pearson", "PearsonDist(x, 12, integrale)", 0, 90);
+  //TF1 *Pearson = new TF1("Pearson", "PearsonDist(x, 12)*integrale", 0, 90);
   
   TCanvas *cchiquadro = new TCanvas("cchiquadro", "Canvas della distribuzione #chi^{2}", 620, 10, 600, 400);
   cchiquadro->cd();
@@ -82,8 +92,18 @@ double CalcolaChi(int n[], double nu[], const int lunghezza){
   return chiquadro;
 }
 
+///*
+double PearsonDist(double x, int gradiliberta, double norm){
+  double nmezzi = gradiliberta/2;
+  
+  return norm * pow(x, nmezzi -1) * Exp(-x/2) / (pow(2, nmezzi) * Gamma(nmezzi));
+}
+//*/
+
+/*
 double PearsonDist(double x, int gradiliberta){
   double nmezzi = gradiliberta/2;
   
   return pow(x, nmezzi -1) * Exp(-x/2) / (pow(2, nmezzi) * Gamma(nmezzi));
 }
+*/
