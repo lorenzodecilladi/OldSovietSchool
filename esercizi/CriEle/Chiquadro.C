6d4ecaf9a16eb91290eadce223f07c0264c66a2c
Eleonora <eleonora.racca@cern.ch>
2017-11-15 17:10:15 +0100

    Ele: funzione per il chi quadro funzionantegit add Chiquadro.C!

--- esercizi/CriEle/Chiquadro.C
+++ esercizi/CriEle/Chiquadro.C
@@ -20,8 +20,7 @@ void ChiquadroAlto(); //Funzione da chiamare per eseguire la macro con fondo alt
 
 double Fondo(int x);
 double CalcolaChi(int n[], double nu[], const int lunghezza);
-double PearsonDist(double x, const int gradiliberta, double normalizzazione);
-//double PearsonDist(double x, int gradiliberta);
+double PearsonDist(double *variabile, double *parametri);
 
 
 // ~~~~~~~~~~~~~~~~~~ Implementazioni ~~~~~~~~~~~~~~~~~~
@@ -33,8 +32,8 @@ void Chiquadro(){
   const int nbin = 12;
   const int nesperimenti = 1e6;
   const double xmin = 0.;
-  const double xmax = 90.;
-  const int nbinchi = 100;
+  const double xmax = 60.;
+  const int nbinchi = 60;
   const double larghezza = (xmax - xmin) / nbinchi;
   
   const int dati[nbin] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
@@ -76,8 +75,9 @@ void Chiquadro(){
 
   double integrale = larghezza * hChiquadro->Integral(xmin, xmax);
   cout << "L'integrale dell'istogramma per la normalizzazione vale: " << integrale << endl;
-  TF1 *fPearson = new TF1("fPearson", "PearsonDist(x, nbin, integrale)", 0, 90);
-  //TF1 *fPearson = new TF1("fPearson", "PearsonDist(x, nbin)*integrale", 0, 90);
+  TF1 *fPearson = new TF1("fPearson", PearsonDist, xmin, xmax, 2);
+  fPearson->SetParameters(nbin, integrale);
+  fPearson->SetParNames("DoF", "normalizzazione");
 
   // Dati simulati: grafici con la distribuzione del chiquadro
   TLine *lChiquadroDati = new TLine(chiquadrodati, 0, chiquadrodati, integrale*0.05);
@@ -101,13 +101,6 @@ void Chiquadro(){
   hChiquadro->Draw("hist e0");
   lChiquadroDati->Draw("same");
   fPearson->Draw("same");
-
-  // Dati originali: calcolo del p-value
-  int nbinchiquadrodati = hChiquadro->GetXaxis()->FindBin(chiquadrodati);
-  double integralepvalue = larghezza * hChiquadro->Integral(nbinchiquadrodati, nbinchi);
-  double pvalue = integralepvalue / integrale;
-
-  cout << "Il p-value dei dati vale: " << pvalue << endl;
 }
 
 
@@ -130,21 +123,15 @@ double CalcolaChi(int n[], double nu[], const int lunghezza){
 
 
 // ------ Funzione con la distribuzione di Pearson -----
-// /*
-double PearsonDist(double x, const int gradiliberta, double normalizzazione){
+double PearsonDist(double *variabile, double *parametri){
+  double x = variabile[0];
+  int gradiliberta = (int)parametri[0];
+  double normalizzazione = parametri[1];
+  
   double nmezzi = gradiliberta/2;
   
   return normalizzazione * pow(x, nmezzi -1) * Exp(-x/2) / (pow(2, nmezzi) * Gamma(nmezzi));
 }
-// */
-
-/*
-double PearsonDist(double x, int gradiliberta){
-  double nmezzi = gradiliberta/2;
-  
-  return pow(x, nmezzi -1) * Exp(-x/2) / (pow(2, nmezzi) * Gamma(nmezzi));
-}
-*/
 
 
 // ----------------- Main della macro 2 ----------------
@@ -154,8 +141,8 @@ void ChiquadroAlto(){
   const int nbin = 12;
   const int nesperimenti = 1e6;
   const double xmin = 0.;
-  const double xmax = 90.;
-  const int nbinchi = 100;
+  const double xmax = 60.;
+  const int nbinchi = 60;
   const double larghezza = (xmax - xmin)/nbinchi;
   
   const int dati[nbin] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23};
@@ -164,25 +151,13 @@ void ChiquadroAlto(){
   
   TF1 *fFondo = new TF1("fFondo", "1000 * 0.169 * (10 - 0.01 * x)", 0, 24);
  
-  TH1I *hOriginale = new TH1I("hOriginale", "Dati originali", nbin, 0, 24);
-  TH1D *hChiquadro = new TH1D("hChiquadro", "Distribuzione del #chi^{2}", nbinchi, xmin, xmax);
-
-  // Dati originali: calcolo del chiquadro e disegno dell'istogramma
-  for(int i = 0; i < nbin; i++){
-    hOriginale->Fill(dati[i], pesi[i]);
-    attesi[i] = Fondo(dati[i]) * 1000;
-  }
-
-  double chiquadrodati = CalcolaChi(pesi, attesi, nbin);
-  cout << "Si ottiene come \u03c7\u00b2 dei dati: " << setprecision(6) << chiquadrodati << endl;
-
-  TCanvas *cOriginale = new TCanvas("cOriginale", "Canvas dei dati originali", 10, 10, 600, 400);
-  cOriginale->cd();
-  hOriginale->SetFillColor(kBlue);
-  hOriginale->Draw("histo");
-  fFondo->Draw("same");
+  TH1D *hChiquadroAlto = new TH1D("hChiquadroAlto", "Distribuzione del #chi^{2}", nbinchi, xmin, xmax);
   
   // Dati simulati: simulazione degli eventi e calcolo dei chiquadro per ogni evento
+  for(int i = 0; i < nbin; i++){
+    attesi[i] = 1000*Fondo(dati[i]);
+  }
+  
   for(int i = 0; i < nesperimenti; i++){
     int datiesperimenti[nbin] = {0};
     double chiquadroesperimenti = 0.;
@@ -192,41 +167,28 @@ void ChiquadroAlto(){
     }
 
     chiquadroesperimenti = CalcolaChi(datiesperimenti, attesi, nbin);
-    hChiquadro->Fill(chiquadroesperimenti);
+    hChiquadroAlto->Fill(chiquadroesperimenti);
   }
 
-  double integrale = larghezza*hChiquadro->Integral(xmin, xmax);
+  double integrale = larghezza*hChiquadroAlto->Integral(xmin, xmax);
   cout << "L'integrale dell'istogramma per la normalizzazione vale: " << integrale << endl;
-  TF1 *fPearson = new TF1("fPearson", "PearsonDist(x, nbin, integrale)", 0, 90);
-  //TF1 *fPearson = new TF1("fPearson", "PearsonDist(x, nbin)*integrale", 0, 90);
-
-  // Dati simulati: grafici con la distribuzione del chiquadro
-  TLine *lChiquadroDati = new TLine(chiquadrodati, 0, chiquadrodati, integrale*0.05);
+  TF1 *fPearson = new TF1("fPearson", PearsonDist, xmin, xmax, 2);
+  fPearson->SetParameters(nbin, integrale);
+  fPearson->SetParNames("DoF", "normalizzazione");
   
-  TCanvas *cChiquadro = new TCanvas("cChiquadro", "Canvas della distribuzione chiquadro con fondo basso", 100, 100, 1200, 400);
-  cChiquadro->Divide(2,1);
+  TCanvas *cChiquadroAlto = new TCanvas("cChiquadroAlto", "Canvas della distribuzione chiquadro con fondo alto", 100, 100, 1200, 400);
+  cChiquadroAlto->Divide(2,1);
   
-  cChiquadro->cd(1);
-  hChiquadro->GetXaxis()->SetTitle("#chi^{2}");
-  hChiquadro->GetYaxis()->SetTitle("Occorrenze\n\n");
-  hChiquadro->SetLineColor(kBlue);
-  hChiquadro->Draw();
-  lChiquadroDati->SetLineColor(kViolet);
-  lChiquadroDati->SetLineWidth(2);
-  lChiquadroDati->Draw("same");
+  cChiquadroAlto->cd(1);
+  hChiquadroAlto->GetXaxis()->SetTitle("#chi^{2}");
+  hChiquadroAlto->GetYaxis()->SetTitle("Occorrenze\n\n");
+  hChiquadroAlto->SetLineColor(kBlue);
+  hChiquadroAlto->Draw();
   fPearson->SetLineColor(kRed);
   fPearson->Draw("same");
 
-  cChiquadro->cd(2);
+  cChiquadroAlto->cd(2);
   gPad->SetLogy();
-  hChiquadro->Draw("hist e0");
-  lChiquadroDati->Draw("same");
+  hChiquadroAlto->Draw("hist e0");
   fPearson->Draw("same");
-
-  // Dati originali: calcolo del p-value
-  int nbinchiquadrodati = hChiquadro->GetXaxis()->FindBin(chiquadrodati);
-  double integralepvalue = larghezza * hChiquadro->Integral(nbinchiquadrodati, nbinchi);
-  double pvalue = integralepvalue / integrale;
-
-  cout << "Il p-value dei dati vale: " << pvalue << endl;
 }
