852a590596407af5a009543439b7e38eac414d7e
Arianna <arianna.corrado@edu.unito.it>
2017-11-28 18:28:19 +0100

    modified .gitignore

--- esercizi/VertexDetector/Sim/compile.C
+++ esercizi/VertexDetector/Sim/compile.C
@@ -27,4 +27,6 @@ void compile(TString myopt="fast"){
 
 
 // schifi da rimuovere ogni volta
+// non più necessario perchè non più tracciati da git (vd .gitignore)
+// per rimuoverli: git clean -x -i  (seguire istruzioni)
 // rm src/Direction_cxx.d src/Direction_cxx.so src/MultipleScattering_cxx.d src/MultipleScattering_cxx.so src/Particle_cxx.d src/Particle_cxx.so src/Point_cxx.d src/Point_cxx.so src/Transport_cxx.d src/Transport_cxx.so src/Vertex_cxx.d src/Vertex_cxx.so sim_C.d sim_C.so
